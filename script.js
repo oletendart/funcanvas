d10d8aa67a40e81eb4ba7554ca2ebd185909ccfd
Olivia Letendart <letendartolivia@gmail.com>
2020-01-21 14:27:28 +0100

    create canvas width and height + ctx join, cap and style

--- script.js
+++ script.js
@@ -3,3 +3,10 @@
 const canvas = document.querySelector(".draw");
 
 const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+ctx.strokeStyle = "#BADA55";
+ctx.lineJoine = "round";
+ctx.lineCap = "round";
