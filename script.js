b744532c90b73fec4627209d4af095a67cc59c14
Olivia Letendart <letendartolivia@gmail.com>
2020-01-21 15:05:55 +0100

    create if or else in the function draw for the direction line

--- script.js
+++ script.js
@@ -16,12 +16,12 @@ let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
 let hue = 0;
+let direction = true;
 
 function draw(e) {
   if (!isDrawing) return;
   console.log(e); // stop the function from running when they are not moused down
   ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
-  ctx.lineWidth = hue;
   ctx.beginPath();
   // start from
   ctx.moveTo(lastX, lastY);
@@ -33,6 +33,14 @@ function draw(e) {
   hue++;
   if (hue >= 360) {
     hue = 0;
+  }
+  if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
+    direction != !direction;
+  }
+  if (direction) {
+    ctx.lineWidth++;
+  } else {
+    ctx.lineWidth--;
   }
 }
 
